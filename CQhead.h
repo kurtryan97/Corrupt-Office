772b91e8b54b74b32d9bd1898f518e5275e0ec2e
JAAFAR JAMILE OMAR <jaafarj.omar@gmail.com>
2022-11-03 16:28:28 +0800

    Implement linked list for both stack and queue. Also, can be use with or without object.

--- CQhead.h
+++ CQhead.h
@@ -11,101 +11,276 @@
 #include <list>
 using namespace std;
 
-class CorruptQueue;
+struct Node {
+    string data;
+    Node* nxt = nullptr;
+    Node() = default;
+};
 
 class CorruptQueue {
-protected:
-    string sv_status = "leave";
 
+protected:
+    bool svStat = false;
 
 public:
-    class RegularQueue {
-    private:
-        string name;
 
+    CorruptQueue() = default;
+
+     void change_stat(const string& xx) {
+        if(xx == "arrive") {
+            svStat = true;
+            cout << "Supervisor present" << endl;
+        } else {
+            svStat = false;
+            cout << "Supervisor not here" << endl;
+        }
+    }
+
+    bool isArrive() const {
+
+        if(svStat){
+            return true;
+        } else {
+            return false;
+        }
+    }
+};
+
+/*
+    *  RegularQueue
+    */
+
+class RegularQueue : public CorruptQueue {
+protected:
+
+public:
+    Node *front, *back;
+    RegularQueue() : front(nullptr), back(nullptr) {}
+
+    virtual void insertQueue(const string &qnm) {
+        Node* insertNode = new Node;
+        insertNode->data = qnm;
+        if(back == nullptr) {
+            front = insertNode;
+            back = insertNode;
+        } else {
+            back->nxt = insertNode;
+            back = insertNode;
+        }
+    }
+
+    virtual void popQueue() {
+        if(front == nullptr) {
+            cout << "Nothing to pop" << endl;
+        } else {
+            Node* popNode = front;
+            front = front->nxt;
+
+            if(front == nullptr) {
+                back = nullptr;
+            }
+
+            delete popNode;
+        }
+    }
+
+    virtual void frontQueue() const {
+        cout << front->data;
+    }
+
+    virtual void backQueue() const {
+        cout << back->data;
+    }
+};
+
+/*
+*  VIP Stack class
+*/
+
+class VIPStack : public CorruptQueue {
+
+public:
+    Node *top;
+    VIPStack() : top(nullptr) {}
+
+    virtual void pushStack(const string &nm) {
+        Node* pushNode = new Node;
+        pushNode->data = nm;
+        pushNode->nxt = nullptr;
+        if(top != nullptr) {
+            pushNode->nxt = top;
+            top = pushNode;
+        } else {
+            top = pushNode;
+            pushNode->nxt = nullptr;
+        }
+    }
+
+    virtual void popStack() {
+        Node* popStackNode;
+        if(top == nullptr) {
+            cout << "Stack is empty" << endl;
+        } else {
+            popStackNode = top;
+            top = top->nxt;
+            delete popStackNode;
+        }
+    }
+
+    virtual void topStack() const {
+        Node* topNode;
+        if(top == nullptr) {
+            cout << "Stack is empty" << endl;
+        } else {
+            topNode = top;
+            cout << topNode->data;
+        }
+    }
+
+    virtual void size_stack() {
+        Node* sizeNode;
+        int i = 0;
+        if(top != nullptr) {
+            sizeNode = top;
+            while(sizeNode) {
+                i++;
+                sizeNode = sizeNode->nxt;
+            }
+            cout << i << endl;
+        } else {
+            cout << "Stack is empty" << endl;
+        }
+    }
+
+    virtual bool isEmpty() {
+        Node *checker;
+        checker = top;
+        if(checker == nullptr) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+};
+
+RegularQueue regular_client;
+VIPStack vip_client;
+CorruptQueue sup_stat;
+
+class CQSimulation {
     public:
-        RegularQueue() {}
-        RegularQueue(string nm) : name(nm) {}
-        void inData(string n) {
-            name = n;
+
+    CQSimulation() = default;
+
+    // without using object
+
+    CQSimulation(const string &s_1, const string &s_2, const string &s_3) {
+        if(sup_stat.isArrive()) {
+            if(s_3 == "regular") {
+                regular_client.insertQueue(s_2);
+                cout << "Regular client " << s_2 << " lines up at RegularQueue" << endl;
+            } else if(s_3 == "VIP") {
+                regular_client.insertQueue(s_2);
+                cout << "VIP client " << s_2 << " lines up at RegularQueue" << endl;
+            }
+        } else {
+            if(s_3 == "regular") {
+                regular_client.insertQueue(s_2);
+                cout << "Regular client " << s_2 << " lines up at RegularQueue" << endl;
+            } else if(s_3 == "VIP") {
+                vip_client.pushStack(s_2);
+                cout << "VIP client " << s_2 << " lines up at VIPStack" << endl;
+            }
         }
+    }
 
-        void outData() {
-            cout << name;
+    CQSimulation(const string &s_1, const string &s_2) {
+        sup_stat.change_stat(s_1);
+        if(sup_stat.isArrive()) {
+            while(!vip_client.isEmpty()) {
+                Node *temp = new Node;
+                temp = vip_client.top;
+                regular_client.insertQueue(temp->data);
+                vip_client.popStack();
+            }
         }
-    };
+    }
 
-    class Pointer {
-    private:
-        RegularQueue rq;
-
-    public:
-        Pointer *nxt;
-        Pointer *prv;
-
-        void setRegQ(RegularQueue r1) {
-            rq = r1;
-        }
-
-        RegularQueue getRegQ() {
-            return rq;
-        }
-    };
-
-
-    /*
-     *  VIP Stack class
-     */
-
-    class VIPStack {
-        int top;
-
-    protected:
-        string name = "";
-
-    public:
-        string vip_client[3];
-
-        VIPStack() {
-            top = -1;
-        }
-
-        void pushStack(string x) {
-            if(top >= 20) {
-                cout << "Stack overflow" << endl;
+    explicit CQSimulation(const string &s_1) {
+        if(sup_stat.isArrive()) {
+            cout << "Now serving ";
+            regular_client.frontQueue();
+            cout << " from RegularQueue" << endl;
+            regular_client.popQueue();
+        } else {
+            if(!vip_client.isEmpty()) {
+                cout << "Now serving ";
+                vip_client.topStack();
+                cout << " from VIPStack" << endl;
+                vip_client.popStack();
             } else {
-                vip_client[++top] = x;
+                cout << "Now serving ";
+                regular_client.frontQueue();
+                cout << " from RegularQueue" << endl;
+                regular_client.popQueue();
             }
         }
+    }
 
-        string popStack() {
-            if(top < 0) {
-                cout << "Stack underflow" << endl;
+    // Using object
+
+    void lineup(const string &s_1, const string &s_2, const string &s_3) {
+        if(sup_stat.isArrive()) {
+            if(s_3 == "regular") {
+                regular_client.insertQueue(s_2);
+                cout << "Regular client " << s_2 << " lines up at RegularQueue" << endl;
+            } else if(s_3 == "VIP") {
+                regular_client.insertQueue(s_2);
+                cout << "VIP client " << s_2 << " lines up at RegularQueue" << endl;
+            }
+        } else {
+            if(s_3 == "regular") {
+                regular_client.insertQueue(s_2);
+                cout << "Regular client " << s_2 << " lines up at RegularQueue" << endl;
+            } else if(s_3 == "VIP") {
+                vip_client.pushStack(s_2);
+                cout << "VIP client " << s_2 << " lines up at VIPStack" << endl;
+            }
+        }
+    }
+
+    void supervisor_status(const string &s_1, const string &s_2) {
+        sup_stat.change_stat(s_1);
+        if(sup_stat.isArrive()) {
+            while (!vip_client.isEmpty()) {
+                Node *temp = new Node;
+                temp = vip_client.top;
+                regular_client.insertQueue(temp->data);
+                vip_client.popStack();
+            }
+        }
+    }
+
+    void serve(const string &s_1) {
+        if(sup_stat.isArrive()) {
+            cout << "Now serving ";
+            regular_client.frontQueue();
+            cout << " from RegularQueue" << endl;
+            regular_client.popQueue();
+        } else {
+            if(!vip_client.isEmpty()) {
+                cout << "Now serving ";
+                vip_client.topStack();
+                cout << " from VIPStack" << endl;
+                vip_client.popStack();
             } else {
-                string d = vip_client[top--];
-                return d;
+                cout << "Now serving ";
+                regular_client.frontQueue();
+                cout << " from RegularQueue" << endl;
+                regular_client.popQueue();
             }
         }
-
-        void topStack() {
-            cout << vip_client[top];
-        }
-
-        void size_stack() {
-            cout << top + 1 << endl;
-        }
-
-        bool isEmpty() {
-            if(top < 0)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-    };
+    }
 };
 
 #endif //CORRUPTQUEUE_CQHEAD_H
--- main.cpp
+++ main.cpp
@@ -1,35 +1,78 @@
-#include "CQhead2.h"
+//
+// Created by Jfr on 26/10/2022.
+//
+#include "CQhead.h"
 
 int main() {
+    // creating an object
+    CQSimulation simulate;
 
-    SQSimulation("lineup","John","regular");
-    SQSimulation("lineup","Bob","regular");
-    SQSimulation("lineup","Tom","regular");
-    SQSimulation("lineup","Sarah","VIP");
-    SQSimulation("lineup","Marie","VIP");
-    SQSimulation("lineup","Joan","VIP");
-    SQSimulation("serve");
-    SQSimulation("serve");
-    SQSimulation("arrive","supervisor");
-    SQSimulation("serve");
-    SQSimulation("serve");
-    SQSimulation("serve");
-    SQSimulation("lineup","Bea","VIP");
-    SQSimulation("lineup","Hank","regular");
-    SQSimulation("serve");
-    SQSimulation("serve");
-    SQSimulation("serve");
-    SQSimulation("leave","supervisor");
-    SQSimulation("lineup","Art","regular");
-    SQSimulation("lineup","Daisy","VIP");
-    SQSimulation("lineup","Marius","regular");
-    SQSimulation("lineup","Dane","VIP");
-    SQSimulation("serve");
-    SQSimulation("arrive","supervisor");
-    SQSimulation("serve");
-    SQSimulation("serve");
-    SQSimulation("serve");
-    SQSimulation("leave","supervisor");
+    /*
+    *     implementation without using an object
+    *     (uncomment/comment this if you want to use/unuse without object).
+    */
+
+    CQSimulation("lineup","John","regular");
+    CQSimulation("lineup","Bob","regular");
+    CQSimulation("lineup","Tom","regular");
+    CQSimulation("lineup","Sarah","VIP");
+    CQSimulation("lineup","Marie","VIP");
+    CQSimulation("lineup","Joan","VIP");
+    CQSimulation("serve");
+    CQSimulation("serve");
+    CQSimulation("arrive","supervisor");
+    CQSimulation("serve");
+    CQSimulation("serve");
+    CQSimulation("serve");
+    CQSimulation("lineup","Bea","VIP");
+    CQSimulation("lineup","Hank","regular");
+    CQSimulation("serve");
+    CQSimulation("serve");
+    CQSimulation("serve");
+    CQSimulation("leave","supervisor");
+    CQSimulation("lineup","Art","regular");
+    CQSimulation("lineup","Daisy","VIP");
+    CQSimulation("lineup","Marius","regular");
+    CQSimulation("lineup","Dane","VIP");
+    CQSimulation("serve");
+    CQSimulation("arrive","supervisor");
+    CQSimulation("serve");
+    CQSimulation("serve");
+    CQSimulation("serve");
+    CQSimulation("leave","supervisor");
+
+    /*
+     *  implementation using object (simulate).
+     *  (uncomment/comment this if you want to use/unuse with object instead).
+     */
+//    simulate.lineup("lineup","John","regular");
+//    simulate.lineup("lineup","Bob","regular");
+//    simulate.lineup("lineup","Tom","regular");
+//    simulate.lineup("lineup","Sarah","VIP");
+//    simulate.lineup("lineup","Marie","VIP");
+//    simulate.lineup("lineup","Joan","VIP");
+//    simulate.serve("serve");
+//    simulate.serve("serve");
+//    simulate.supervisor_status("arrive","supervisor");
+//    simulate.serve("serve");
+//    simulate.serve("serve");
+//    simulate.serve("serve");
+//    simulate.lineup("lineup","Bea","VIP");
+//    simulate.lineup("lineup","Hank","regular");
+//    simulate.serve("serve");
+//    simulate.serve("serve");
+//    simulate.serve("serve");
+//    simulate.supervisor_status("leave","supervisor");
+//    simulate.lineup("lineup","Art","regular");
+//    simulate.lineup("lineup","Daisy","VIP");
+//    simulate.lineup("lineup","Marius","regular");
+//    simulate.lineup("lineup","Dane","VIP");
+//    simulate.serve("serve");
+//    simulate.supervisor_status("arrive","supervisor");
+//    simulate.serve("serve");
+//    simulate.serve("serve");
+//    simulate.serve("serve");
+//    simulate.supervisor_status("leave","supervisor");
 
     return 0;
 
